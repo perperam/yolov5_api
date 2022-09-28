b5d9360ce6fe471bdfdf720f61ba54b21c87da82
robin <83653446+perperam@users.noreply.github.com>
2022-09-28 20:06:37 +0200

    changed default model to yolov5n

--- restapi.py
+++ restapi.py
@@ -1,4 +1,4 @@
-# YOLOv5 by Ultralytics, GPL-3.0 license
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
 """
 Run a Flask REST API exposing one or more YOLOv5s models
 """
@@ -39,7 +39,7 @@ def predict(model):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
     parser.add_argument("--port", default=5000, type=int, help="port number")
-    parser.add_argument('--model', nargs='+', default=['yolov5s'], help='model(s) to run, i.e. --model yolov5n yolov5s')
+    parser.add_argument('--model', nargs='+', default=['yolov5n'], help='model(s) to run, i.e. --model yolov5n yolov5s')
     opt = parser.parse_args()
 
     for m in opt.model:
